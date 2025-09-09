34ae919d5351beb224733ef88a2f38ca4c677ea8
infj294 <rkeldjs9@gmail.com>
2025-09-09 11:21:34 +0900

    Add files via upload
    0909

---
+++ ch02/src/sec02/ex01.java
@@ -0,0 +1,21 @@
+package sec02;
+
+public class ex01 {
+
+	public static void main(String[] args) {
+		byte byteVal = 10;
+		int intVal = byteVal;
+		///byte byteVall = intVal;
+		
+		char charVal = 'A';
+		//char charVal = 65;
+		//System.out.println(charVal);
+		int intVall = charVal;
+	    System.out.println((char)intVall);
+	    
+	    byte byteVal1 = 65;
+	    int intVal2 = 65;
+	    char charVal2 = (char)byteVall;
+	    char charVAl3 = byteVall;
+	}
+}
