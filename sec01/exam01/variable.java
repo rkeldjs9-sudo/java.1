be1caa6acb475ba250705bef9ad6681a251858f7
infj294 <rkeldjs9@gmail.com>
2025-09-23 10:49:20 +0900

    09-23
    09-23

---
+++ sec01/exam01/variable.java
@@ -0,0 +1,17 @@
+package sec01.exam01;
+
+public class variable {
+	public static void main(String[] args) {
+		int a= 0;
+		int b = a;
+		
+		string str1 = null;
+		string str2 = str1;
+		
+		System.out.println("hi.");
+		
+		/*int[] int Array = null;
+		intArray[0] = 10;*/
+	}
+
+}
